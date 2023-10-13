e6e6e37744b0bcf8a3a32794107305e09b2c3b68
Tomas LLull <89158964+T0t04d@users.noreply.github.com>
2023-10-13 06:34:55 -0300

    Ahora si que si, popups

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
-import GeoJSON from 'ol/format/GeoJSON';
 import { fromLonLat } from 'ol/proj';
 import { Style, Icon } from 'ol/style';
+import GeoJSON from 'ol/format/GeoJSON';
 
 // Configura el mapa
 const map = new Map({
@@ -19,36 +19,46 @@ const map = new Map({
     }),
   ],
   view: new View({
-    center: fromLonLat([-57.542611, -38.005477]), // Centro del mapa (coordenadas iniciales)
-    zoom: 12, // Nivel de zoom inicial
+    center: fromLonLat([-57.542611, -38.005477]),
+    zoom: 12,
   }),
 });
 
+// Capa de marcadores
+const markerLayer = new VectorLayer({
+  source: new VectorSource(),
+  url: './data.geojson',
+      format: new GeoJSON(),
+});
+map.addLayer(markerLayer);
+
+// Agregar un evento de clic al mapa para manejar los marcadores interactivos
+map.on('singleclick', function (event) {
+  map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
+    // Verifica que se hizo clic en un marcador
+    if (feature) {
+      // Define la acción que se ejecutará al hacer clic en el marcador
+      // Por ejemplo, muestra una alerta con las coordenadas
+      const coords = feature.getGeometry().getCoordinates();
+      alert(`Marcador clickeado en coordenadas: ${coords}`);
+    }
+  });
+});
 
 // Configura un estilo para los marcadores
 const markerStyle = new Style({
   image: new Icon({
     anchor: [0.5, 1],
     src: 'icon.png',
+    scale: 0.02,
     // Ruta a la imagen del marcador
   }),
 });
-
-// Capa de marcadores
-const markerLayer = new VectorLayer({
-  source: new VectorSource({
-      url: './data.geojson',
-      format: new GeoJSON(),
-
-  })
-});
-map.addLayer(markerLayer);
 
 
 
 // Manejo del botón "Guardar Marcador"
 const guardarButton = document.getElementById('guardar');
-
 guardarButton.addEventListener('click', () => {
   const latitudInput = document.getElementById('latitud').value;
   const longitudInput = document.getElementById('longitud').value;
@@ -69,5 +79,24 @@ guardarButton.addEventListener('click', () => {
       markerLayer.getSource().addFeature(marker);
     }
   }
+
+marker.setStyle(markerStyle(0.5)); // Configura el estilo del marcador (como en el ejemplo anterior)
+
+// Agrega un evento de clic al marcador
+marker.on('click', function (event) {
+  // Define la acción que se ejecutará al hacer clic en el marcador
+  // Por ejemplo, muestra una alerta con las coordenadas
+  const coords = event.target.getGeometry().getCoordinates();
+  alert(`Marcador clickeado en coordenadas: ${coords}`);
 });
+
+
+// Agrega el marcador a la capa de marcadores
+markerLayer.getSource().addFeature(marker);
+});
+
+
+
+
+
 
